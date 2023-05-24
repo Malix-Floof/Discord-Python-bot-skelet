dc92f1242aba8398e597c08d264ebeba2a387817
Malix <101697781+Malix-Dev@users.noreply.github.com>
2023-05-24 14:05:28 +0300

    Исправление ошибок

--- bot.py
+++ bot.py
@@ -1,16 +1,21 @@
-import discord #Импортируем библиотеку "discord" // Import the "discord" library
-from discord.ext import commands
+import discord #Импортируем библиотеку "disnake" // Import the "disnake" library
+from disnake.ext import commands
+from config import settings
 
-#Ваш префикс бота (по умолчанию префикс "!") // Your bot prefix (default prefix "!")
-bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+class TestBot(commands.Bot):
+    def __init__(self, *args,  **kwargs):
+        super().__init__(*args, **kwargs)
 
-@bot.event
-async def on_ready():
-	print("Bot Ready!")
+    async def on_ready(self):
+        print("Запущен!")
+
+intents = disnake.Intents.all()
+bot = MyBot(command_prefix=settings['PREFIX'], intents = intents) #Ваш префикс бота (по умолчанию префикс "!") // Your bot prefix (default prefix "!")
 
 @bot.command()
 async def test(ctx):
 	await ctx.send("Привет! // Hello!")
-	print("The test command has been executed ✔")
+	print("Тест команда была успешно выполнена // The test command has been executed")
 
-bot.run("Ваш токен бота // You Bot Token")
+ # Токен бота можно получить на https://discord.com/developers/applications // Bot token available at https://discord.com/developers/applications
+bot.run(settings['TOKEN'])
